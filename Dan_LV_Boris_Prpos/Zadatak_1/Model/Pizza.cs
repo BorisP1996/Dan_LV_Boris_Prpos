e4fa13b6fad6cbd88b1f3d9b8e47505a3d3a04f1
BorisP1996 <prposboris13@hotmail.com>
2020-08-24 13:11:13 +0200

    designed main window

---
+++ Dan_LV_Boris_Prpos/Zadatak_1/Model/Pizza.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Model
+{
+    class Pizza
+    {
+        public int smallPiza = 200;
+        public int mediumPiza = 400;
+        public int biggPiza = 600;
+        public int salama = 100;
+        public int kulen = 150;
+        public int kecap = 40;
+        public int sunka = 120;
+        public int majonez = 40;
+        public int paprika = 55;
+        public int masline = 61;
+        public int origano = 34;
+        public int susam = 14;
+        public int sir = 84;
+
+        public bool smallPizab;
+        public bool mediumPizab;
+        public bool biggPizab;
+        public bool kulenb;
+        public bool kecapb;
+        public bool sunkab;
+        public bool majonezb;
+        public bool paprikab;
+        public bool maslineb;
+        public bool origanob;
+        public bool susamb;
+        public bool sirb;
+
+        private bool salamab;
+
+        public bool SalamaB
+        {
+            get { return salamab; }
+            set { salamab = value; }
+        }
+
+    }
+}
---
+++ Dan_LV_Boris_Prpos/Zadatak_1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Zadatak_1.Model;
+
+namespace Zadatak_1.ViewModel
+{
+    class MainWindowViewModel:ViewModelBase
+    {
+        MainWindow main;
+
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+            Pizza = new Pizza();
+        }
+
+        private Pizza pizza;
+
+        public Pizza Pizza
+        {
+            get { return pizza; }
+            set { pizza = value;
+                OnPropertyChanged("Pizza");
+            }
+        }
+
+
+        private int totalPrice;
+
+        public int TotalPrice
+        {
+            get { return totalPrice; }
+            set { totalPrice = value; }
+        }
+
+        private string labelMessage;
+
+        public string LabelMessage
+        {
+            get { return labelMessage; }
+            set { labelMessage = value;
+                OnPropertyChanged("LabelMessage");
+            }
+        }
+
+
+        private ICommand calculateAmount;
+        public ICommand CalculateAmount
+        {
+            get
+            {
+                if (calculateAmount==null)
+                {
+                    calculateAmount = new RelayCommand(param => CalculateAmountExecute(), param => CanCalculateAmountExecute());
+                }
+                return calculateAmount;
+            }
+        }
+
+        private bool CanCalculateAmountExecute()
+        {
+            return true;
+        }
+
+        private void CalculateAmountExecute()
+        {
+            Pizza newPiza = new Pizza();
+            newPiza.SalamaB = Pizza.SalamaB;
+            if (newPiza.SalamaB==true)
+            {
+                LabelMessage = newPiza.salama.ToString();
+            }
+        }
+    }
+}
